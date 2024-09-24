6f05af3a5d402f2dcf2834fb5aada3177274587d
gabrmanh <gmanhani@yahoo.com>
2024-09-23 23:06:44 -0300

    test: first valid test for DroneDeliveryService

--- src/test/java/DroneDeliveryServiceTest.java
+++ src/test/java/DroneDeliveryServiceTest.java
@@ -1,11 +1,18 @@
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class DroneDeliveryServiceTest {
     private final DroneDeliveryService sut = new DroneDeliveryService();
     
     @Test
+    @DisplayName("Should return 'S' if the box can fit in the window given box values smaller than the window")
     public void deliverSuccess() {
         
+        char result = sut.deliver(1,2,3,4,5);
+        
+        assertThat(result).isEqualTo('S');
     }
     
 }
