a7b24f2f1db70ac31f6fc5251e2ade3c8fec5044
gabrmanh <gmanhani@yahoo.com>
2024-09-23 23:30:18 -0300

    test: third invalid test

--- src/test/java/DroneDeliveryServiceTest.java
+++ src/test/java/DroneDeliveryServiceTest.java
@@ -51,6 +51,12 @@ public class DroneDeliveryServiceTest {
             char result = sut.deliver(80, 90, 10, 2, 3);
             assertThat(result).isEqualTo('N');
         }
-
+        
+        @Test
+        @DisplayName("Should throw IllegalArgumentException if any of the values is zero")
+        public void deliverFailureZero(){
+            char result = sut.deliver(10, 20, 30, 1, 0);
+            assertThatThrownBy(() -> sut.deliver(10, 20, 30, 1, 0)).isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
