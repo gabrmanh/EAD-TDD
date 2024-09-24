a2548d955e40ca6dfe9bf9e2b289976e92b338ad
gabrmanh <gmanhani@yahoo.com>
2024-09-23 23:14:19 -0300

    test: second valid test and nesting

--- src/test/java/DroneDeliveryServiceTest.java
+++ src/test/java/DroneDeliveryServiceTest.java
@@ -1,18 +1,29 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class DroneDeliveryServiceTest {
     private final DroneDeliveryService sut = new DroneDeliveryService();
-    
-    @Test
-    @DisplayName("Should return 'S' if the box can fit in the window given box values smaller than the window")
-    public void deliverSuccess() {
-        
-        char result = sut.deliver(1,2,3,4,5);
-        
-        assertThat(result).isEqualTo('S');
+
+    @Nested
+    @DisplayName("Valid Classes")
+    class ValidClasses {
+
+        @Test
+        @DisplayName("Should return 'S' if the box can fit in the window given box values smaller than the window")
+        public void deliverSuccess() {
+            char result = sut.deliver(1, 2, 3, 4, 5);
+            assertThat(result).isEqualTo('S');
+        }
+
+        @Test
+        @DisplayName("Should return 'S' if two of the box's sides are smaller than the window")
+        public void deliverSuccessTwoSmaller()
+        {
+            char result = sut.deliver(10, 5, 3, 6, 4);
+            assertThat(result).isEqualTo('S');
+        }
     }
-    
 }
